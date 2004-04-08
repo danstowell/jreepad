5f2888194148c5df879709a8bda44ab3e94fed7a
Dan Stowell <danstowell@users.sourceforge.net>
2004-04-08 20:26:03 +0000

    Separated off JreepadTreeModel class
    git-svn-id: https://jreepad.svn.sourceforge.net/svnroot/jreepad/trunk@19 551eb26d-2702-42b4-948d-f9171e4fbe8c

---
+++ jreepad/JreepadTreeModel.java
@@ -0,0 +1,184 @@
+package jreepad;
+
+import javax.swing.*;
+import javax.swing.tree.*;
+import javax.swing.event.*;
+import java.awt.event.*;
+
+  public class JreepadTreeModel implements TreeModel
+  {
+    private JreepadNode root;
+    protected EventListenerList listenerList = new EventListenerList();
+    public JreepadTreeModel(JreepadNode root)
+    {
+      this.root = root;
+    }
+    public JreepadTreeModel()
+    {
+      this(new JreepadNode());
+    }
+    public Object getChild(Object parent, int index)
+    {
+      return ((JreepadNode)parent).getChildAt(index);
+    }
+    public int getChildCount(Object parent)
+    {
+      return ((JreepadNode)parent).getChildCount();
+    }
+    public int getIndexOfChild(Object parent, Object child)
+    {
+      return ((JreepadNode)parent).getIndex((JreepadNode)child);
+    }
+    public Object getRoot()
+    {
+      return root;
+    }
+    public boolean isLeaf(Object node)
+    {
+      return ((JreepadNode)node).isLeaf();
+    }
+    public void addTreeModelListener(TreeModelListener l) {
+        listenerList.add(TreeModelListener.class, l);
+    }
+    public void removeTreeModelListener(TreeModelListener l) {
+        listenerList.remove(TreeModelListener.class, l);
+    }
+    public void valueForPathChanged(TreePath path, Object newValue)
+    {
+      ((JreepadNode)path.getLastPathComponent()).setTitle((String)newValue);
+    }
+    
+    // Fire notifications - copied directly from DefaultTreeModel
+    /**
+      * Invoke this method after you've changed how node is to be
+      * represented in the tree.
+      */
+    public void nodeChanged(TreeNode node) {
+        if(listenerList != null && node != null) {
+            TreeNode         parent = node.getParent();
+
+            if(parent != null) {
+                int        anIndex = parent.getIndex(node);
+                if(anIndex != -1) {
+                    int[]        cIndexs = new int[1];
+
+                    cIndexs[0] = anIndex;
+                    nodesChanged(parent, cIndexs);
+                }
+            }
+	    else if (node == getRoot()) {
+		nodesChanged(node, null);
+	    }
+        }
+    }
+    /**
+      * Invoke this method after you've inserted some TreeNodes into
+      * node.  childIndices should be the index of the new elements and
+      * must be sorted in ascending order.
+      */
+    public void nodesWereInserted(TreeNode node, int[] childIndices) {
+        if(listenerList != null && node != null && childIndices != null
+           && childIndices.length > 0) {
+            int               cCount = childIndices.length;
+            Object[]          newChildren = new Object[cCount];
+
+            for(int counter = 0; counter < cCount; counter++)
+                newChildren[counter] = node.getChildAt(childIndices[counter]);
+            fireTreeNodesInserted(this, getPathToRoot(node), childIndices, 
+                                  newChildren);
+        }
+    }
+    
+    /**
+      * Invoke this method after you've removed some TreeNodes from
+      * node.  childIndices should be the index of the removed elements and
+      * must be sorted in ascending order. And removedChildren should be
+      * the array of the children objects that were removed.
+      */
+    public void nodesWereRemoved(TreeNode node, int[] childIndices,
+                                 Object[] removedChildren) {
+        if(node != null && childIndices != null) {
+            fireTreeNodesRemoved(this, getPathToRoot(node), childIndices, 
+                                 removedChildren);
+        }
+    }
+
+    protected void fireTreeNodesChanged(Object source, Object[] path, 
+                                        int[] childIndices, 
+                                        Object[] children) {
+        // Guaranteed to return a non-null array
+        Object[] listeners = listenerList.getListenerList();
+        TreeModelEvent e = null;
+        // Process the listeners last to first, notifying
+        // those that are interested in this event
+        for (int i = listeners.length-2; i>=0; i-=2) {
+            if (listeners[i]==TreeModelListener.class) {
+                // Lazily create the event:
+                if (e == null)
+                    e = new TreeModelEvent(source, path, 
+                                           childIndices, children);
+                ((TreeModelListener)listeners[i+1]).treeNodesChanged(e);
+            }          
+        }
+    }
+    protected void fireTreeNodesInserted(Object source, Object[] path, 
+                                        int[] childIndices, 
+                                        Object[] children) {
+        // Guaranteed to return a non-null array
+        Object[] listeners = listenerList.getListenerList();
+        TreeModelEvent e = null;
+        // Process the listeners last to first, notifying
+        // those that are interested in this event
+        for (int i = listeners.length-2; i>=0; i-=2) {
+            if (listeners[i]==TreeModelListener.class) {
+                // Lazily create the event:
+                if (e == null)
+                    e = new TreeModelEvent(source, path, 
+                                           childIndices, children);
+                ((TreeModelListener)listeners[i+1]).treeNodesInserted(e);
+            }          
+        }
+    }
+    protected void fireTreeNodesRemoved(Object source, Object[] path, 
+                                        int[] childIndices, 
+                                        Object[] children) {
+        // Guaranteed to return a non-null array
+        Object[] listeners = listenerList.getListenerList();
+        TreeModelEvent e = null;
+        // Process the listeners last to first, notifying
+        // those that are interested in this event
+        for (int i = listeners.length-2; i>=0; i-=2) {
+            if (listeners[i]==TreeModelListener.class) {
+                // Lazily create the event:
+                if (e == null)
+                    e = new TreeModelEvent(source, path, 
+                                           childIndices, children);
+                ((TreeModelListener)listeners[i+1]).treeNodesRemoved(e);
+            }          
+        }
+    }
+    protected void fireTreeStructureChanged(Object source, Object[] path, 
+                                        int[] childIndices, 
+                                        Object[] children) {
+        // Guaranteed to return a non-null array
+        Object[] listeners = listenerList.getListenerList();
+        TreeModelEvent e = null;
+        // Process the listeners last to first, notifying
+        // those that are interested in this event
+        for (int i = listeners.length-2; i>=0; i-=2) {
+            if (listeners[i]==TreeModelListener.class) {
+                // Lazily create the event:
+                if (e == null)
+                    e = new TreeModelEvent(source, path, 
+                                           childIndices, children);
+                ((TreeModelListener)listeners[i+1]).treeStructureChanged(e);
+            }          
+        }
+    }
+    
+    public void insertNodeInto(JreepadNode child, JreepadNode parent, int index)
+    {
+      // Does this function need to remove the child from its prior location?
+    //  parent.
+    }
+  } // End of: class JreepadTreeModel
