39b067fa31687014e56a720a61bed56ef8862662
danstowell = Dan Stowell <danstowell@users.sourceforge.net>
2007-01-20 13:01:35 +0000

    Added header comment
    git-svn-id: https://jreepad.svn.sourceforge.net/svnroot/jreepad/trunk@107 551eb26d-2702-42b4-948d-f9171e4fbe8c

--- jreepad/src/jreepad/io/XmlWriter.java
+++ jreepad/src/jreepad/io/XmlWriter.java
@@ -1,3 +1,22 @@
+/*
+           Jreepad - personal information manager.
+           Copyright (C) 2004-2006 Dan Stowell
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+The full license can be read online here:
+
+           http://www.gnu.org/copyleft/gpl.html
+*/
+
 package jreepad.io;
 
 import java.io.IOException;
